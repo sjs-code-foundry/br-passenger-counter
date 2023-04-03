fbc9b1b429030f49175bad24d44f3c93c3e70071
sjs-code-foundry <bds.stephen@gmail.com>
2023-04-03 15:22:18 +0100

    2023/04/03 15:20

--- index.js
+++ index.js
@@ -5,23 +5,35 @@ let countCurr = 0
 let countTot = 0
 
 function increment() {
+    console.log("Increment button clicked.")
     countCurr += 1
     countEl.textContent = countCurr
+
+    message = "Current count: " + countCurr
+    console.log(message)
 }
 
 function save() {
+    console.log("Save button clicked.")
     let countStr = countCurr + " - "
     saveEl.textContent += countStr
     countTot += countCurr
     totalEl.textContent = countTot
     countEl.textContent = 0
     countCurr = 0
+
+    message = "Total counted: " + countTot
+    console.log(message)
 }
 
 function reset() {
+    console.log("Reset button clicked.")
     saveEl.textContent = "Previous entries: "
     countEl.textContent = 0
     totalEl.textContent = 0
     countCurr = 0
     countTot = 0
+
+    message = "Current Count & Total Count: " + countCurr + " - " + countTot
+    console.log(message)
 }
